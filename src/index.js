9565dc3c7242e96ad339179da0a96c8e801e975b
VladKhrlmv <kharlamov-vlad03@mail.ru>
2019-03-16 20:23:49 +0300

    feat: return false or true

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  str = str + " ";
+  for (let j = 0 ; j < bracketsConfig.length*bracketsConfig.length; j++) { 
+
+    for (let i = 0 ; i < bracketsConfig.length; i++) {
+      
+      for (let q = 0 ; q < str.length ; q++) {
+        let checkStr = bracketsConfig[i][0] + bracketsConfig[i][1];
+        let checkStrIndex = str.indexOf(checkStr);
+
+        if (checkStrIndex != -1) {
+          str = str.split(checkStr).join('');
+        }
+      }
+    }
+  }
+  if (str == " ")
+    return true;
+  else return false;
 }
